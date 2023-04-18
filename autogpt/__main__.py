e19eb7ba83f2666f74e99828b9dbc14676e2a6dc
caiwenhao <caiwenhao186@live.com>
2023-04-18 14:57:13 +0000

    1

--- autogpt/__main__.py
+++ autogpt/__main__.py
@@ -427,7 +427,7 @@ class Agent:
                 if cfg.debug_mode:
                     print(f"向openai 发起prompt:\n{self.prompt}\n{self.user_input}")
                     
-                response = self.custom_api.talk(prompt=f"{self.prompt}\n{self.user_input}", model=self.model, parent_message_id=self.last_message_id, conversation_id=self.conversation_id)
+                response = self.custom_api.talk(prompt=f"{self.prompt}\n\n User: {self.user_input}", model=self.model, parent_message_id=self.last_message_id, conversation_id=self.conversation_id)
                 assistant_reply = response["message"]["content"]["parts"][0]
                 self.last_message_id=response["message"]["id"]
 
@@ -520,9 +520,9 @@ class Agent:
 
 
             if result is not None:
-                self.prompt(f"{result}")
+                self.prompt(f"System: {result}")
             else:
-                self.prompt(f"Unable to execute command")
+                self.prompt(f"System: Unable to execute command")
 
 
 if __name__ == "__main__":
